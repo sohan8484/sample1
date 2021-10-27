c235d108c490407b81fe2deb1e4f9d0488419709
SOHAN <SOHAN@SOHAN-PC>
2021-10-27 11:16:54 +0530

    image_uploads

--- Zoho_CRM/src/test/java/testScript/TC_1CreateContact.java
+++ Zoho_CRM/src/test/java/testScript/TC_1CreateContact.java
@@ -16,6 +16,7 @@ public class TC_1CreateContact {
 	@Test
 	/*
 	 * author @ sohan
+	 * hard coding
 	 */
 	/**
 	 * automate the vtiger application on creating organization and on creating new contact
